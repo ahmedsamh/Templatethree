10324b0c5517722db8e8131beaeed046040153c5
ahmed sameh <98563299+ahmedsamh@users.noreply.github.com>
2022-09-06 23:42:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,75 @@
+let span = document.querySelector(".up");
+/////////////////////////////////////
+let section = document.querySelector(".our-skills");
+let spans  =  document.querySelectorAll(".our-skills .skill .the-progress span");
+///////////////////////////////////////////////////////////////////////////////////
+let numb = document.querySelectorAll(".stats .box .number");
+let statsSection = document.querySelector(".stats");
+let started = false; // Function Started ? No
+
+
+
+window.onscroll = function () {
+if (this.scrollY >= 840){
+span.classList.add("show");
+}
+else {
+span.classList.remove("show");
+}
+ if (window.scrollY >= section.offsetTop){
+  spans.forEach((span)=>{
+    span.style.width = span.dataset.width;
+  });
+  }
+  if (window.scrollY >= statsSection.offsetTop) {
+    if (!started) {
+    numb.forEach((num) => startCount(num));
+    }
+    started = true;
+}
+}
+span.onclick = function (){
+window.scrollTo({
+top :0,
+behavior:"smooth",
+});
+};
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+/////////////////////////////////////////////////
+// The End Of The Year Date To Countdown To
+// 1000 milliseconds = 1 Second
+let MyDate = new Date("Dec 30, 2022 23:59:59");
+let countDownDate = MyDate.getTime();
+// console.log(countDownDate); 
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".events .container .info .time .unit .days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".events .container .info .time .unit .hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".events .container .info .time .unit .minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".events .container .info .time .unit .seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  if (dateDiff < 0) {
+  clearInterval(counter);
+  }
+}, 1000);
+
